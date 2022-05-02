eb0757fa48b7dbdceed9d95d429816a093d09047
olesatanya <olesatanya@gmail.com>
2022-05-02 02:20:44 -0700

    init : project

---
+++ src/@types/server.d.ts
@@ -0,0 +1,20 @@
+
+declare interface ServerResponse {
+    result?: any
+    error?: number
+}
+
+declare interface ResultType {
+    err: string,
+    result: string
+}
+
+interface WalletTypes extends NewtxsResponseType {
+    updated: number
+    loading?: boolean
+    currentPage: string
+}
+interface useStoreTypes extends WalletTypes {
+    update(payload: { [key: string]: string | number | boolean | PendingTypes | TxTypes | CoinTypes })
+    call(url: string, params?: any): Promise<ServerResponse | null>
+}
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import useStore from '../useStore'
+import {TF, tips,  proxy} from '../util';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Card from '../components/Card'
+import {  Binance, Ethereum, Close, Twitter, Telegram, Website, Discord } from '../components/Icons'
+
+import '../assets/scss/account.scss'
+
+const CoinAge =  () => { 
+
+}
+
+export default CoinAge
